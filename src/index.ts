b1ab8e8b711f6774693bdb812dcf5ac6ca545bfc
Mastagoon <inzilagiway@gmail.com>
2022-09-12 10:36:45 +0300

    fixed upload bug

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv"
 import path from "path"
 import express from "express"
-import { UploadedFile } from "express-fileupload"
+import fileUpload, { UploadedFile } from "express-fileupload"
 dotenv.config()
 
 const FILE_NAME = "clientinfo.xml"
@@ -10,27 +10,30 @@ const filepath = path.join(__dirname, "..", "file", FILE_NAME)
 const main = async () => {
   const PORT = process.env.PORT || 8080
   const app = express()
+  app.use(fileUpload())
   app.listen(PORT, () => console.log(`Server running on ${PORT}`))
 
   app.get("/", (_req, res) => {
-    console.log("Download request")
+    console.log("Download Request...")
     return res.download(filepath)
   })
 
   app.post("/upload", (req, res) => {
-    console.log("RECIEVING FILE")
-    console.log(req.files)
+    console.log("Uplaod Request...")
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).send("No files were uploaded.")
     }
-    const { files } = req
-    const file = files.file as UploadedFile
-    console.log("files recieved")
-    console.log(file)
-    return file.mv(filepath, (err) => {
-      if (err) return res.status(500).send(err)
-      return res.status(201).send("file updated.")
-    })
+    try {
+      const { files } = req
+      const file = files.sampleFile as UploadedFile
+      return file.mv(filepath, (err) => {
+        if (err) return res.status(500).send(err)
+        return res.status(201).send("file updated.")
+      })
+    } catch (err) {
+      console.log("err encountered")
+      console.log(err)
+    }
   })
 }
 
